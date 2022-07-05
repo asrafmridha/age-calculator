c1b6a95aea4169095aad43e5ba397f5f8b7a94f7
asraf-mridha <asrafmridha@gmail.com>
2022-07-05 08:40:28 +0600

    to take data from user & store

--- script.js
+++ script.js
@@ -4,8 +4,22 @@
    var _psntYear= psntDate.getFullYear();
    var _psntmonth=psntDate.getMonth()+1;
    var _psntdate=psntDate.getDate();
+//To Show present date in form
 
    document.getElementById("psntyear").value=_psntYear;
    document.getElementById("psntmonth").value=_psntmonth;
    document.getElementById("psndate").value=_psntdate;
+
+   //To date from user
+
+    var _todate=document.getElementById("todate").value;
+    var _tomonth=document.getElementById("tomonth").value;
+    var _toyear=document.getElementById("toyear").value;
+
+//    function agecalculate(){
+
+    
+
+//     if( _psntYear> )
+//    }
 
